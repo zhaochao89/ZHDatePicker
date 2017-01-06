b7bda9e735edbde84ad92f51843b600309855217
zhaochao <zhaochao@lingdanet.com>
2017-01-06 19:51:04 +0800

    ViewController.m删除一些代码

--- 日期选择器/ViewController.m
+++ 日期选择器/ViewController.m
@@ -18,15 +18,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
-    
-    NSDate *now = [NSDate date];
-    
-    NSInteger day = [calendar component:NSCalendarUnitWeekday fromDate:now];
-    
-    NSDateComponents *components = [calendar components:NSCalendarUnitMonth fromDate:now];
-    
-    NSDate *date = [calendar dateByAddingComponents:components toDate:now options:0];
     
     ZHDatePicker *datePicker = [[ZHDatePicker alloc] initWithFrame:CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, 200)];
     [self.view addSubview:datePicker];
